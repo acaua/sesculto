81a03e9bdde6e04813a366772e75ad3fd6286286
Acauã Sperl de Faria <acaua.faria@gmail.com>
2025-03-17 14:03:22 -0300

    feat: update num activities found

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -38,7 +38,9 @@ export default function Activities() {
       ) : (
         <div>
           <p className="mb-4 text-gray-500">
-            {filteredActivities.length} atividades encontradas
+            {filteredActivities.length < activities.length &&
+              `${filteredActivities.length} de `}
+            {activities.length} atividades encontradas
           </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {filteredActivities.map((activity) => (
