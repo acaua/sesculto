ea6e325974f59d80cebe905d5f09e2609adb5c93
Acauã Sperl de Faria <acaua.faria@gmail.com>
2025-04-24 17:57:52 -0300

    feat: add goat counter analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sesculto.acaua.dev",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
